8e974a74e0b5ec3bc5658effc261876637d80042
Yegor Gangalo <gangaloyegor@gmail.com>
2021-01-21 00:16:44 +0200

    fixed fetch problem in imagegallery add page!==1

--- src/APIservice/APIservice.js
+++ src/APIservice/APIservice.js
@@ -1,18 +1,16 @@
 const staticFetchOptions = {
-    perPage: 12,
-    staticURL: 'https://pixabay.com/api/?image_type=photo&orientation=horizontal',
-    keyAPI: '19532775-cd1fec64673db4c80a00103d2',
-}
+  perPage: 12,
+  staticURL: 'https://pixabay.com/api/?image_type=photo&orientation=horizontal',
+  keyAPI: '19532775-cd1fec64673db4c80a00103d2',
+};
 
 const { staticURL, perPage, keyAPI } = staticFetchOptions;
 
-export function imageAPI (query, page) {
-    const url = `${staticURL}&q=${query}&page=${page}&per_page=${perPage}&key=${keyAPI}`;
-    return fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                return Promise.reject(new Error(`There is no image with tag ${query}`));
-            }
-            return response.json();
-        })
+export function imageAPI(query, page) {
+  const url = `${staticURL}&q=${query}&page=${page}&per_page=${perPage}&key=${keyAPI}`;
+  return fetch(url).then(response => {
+    return response.ok
+      ? response.json()
+      : Promise.reject(new Error(`There is no image with tag ${query}`));
+  });
 }
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,108 +1,116 @@
 import { useState, useEffect, useRef } from 'react';
 import { imageAPI } from '../../APIservice';
-import Notification from '../Notification'
+import Notification from '../Notification';
 import ImageGalleryList from '../ImageGalleryList';
 import Button from '../Button';
 import PropTypes from 'prop-types';
 
 const { IDLE, PENDING, REJECTED, RESOLVED } = {
-    IDLE: 'idle',
-    PENDING: 'pending',
-    REJECTED: 'rejected',
-    RESOLVED: 'resolved',
-}
+  IDLE: 'idle',
+  PENDING: 'pending',
+  REJECTED: 'rejected',
+  RESOLVED: 'resolved',
+};
 
-function ImageGallery ({searchQuery}) {
+function ImageGallery({ searchQuery }) {
+  const [status, setStatus] = useState(IDLE);
+  const [imgFetched, setImgFetched] = useState([]);
+  const [imgTotal, setImgTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const [error, setError] = useState('');
 
-    const [status, setStatus] = useState(IDLE);
-    const [imgFetched, setImgFetched] = useState([]);
-    const [imgTotal, setImgTotal] = useState(0);
-    const [page, setPage] = useState(1);
-    const [error, setError] = useState('');
+  useEffect(() => {
+    resetGallery();
+  }, [searchQuery]);
 
-    useEffect(() => {
-        resetGallery();
-    },[searchQuery])
+  const oldQuery = useRef('');
 
-    const oldQuery = useRef('');
+  useEffect(() => {
+    if (!searchQuery.trim()) {
+      return;
+    }
+    if (page !== 1 && oldQuery.current !== searchQuery) {
+      return;
+    }
 
-    useEffect(() => {
-        if (!searchQuery.trim()) { return };
-        if (!oldQuery.current) { oldQuery.current = searchQuery };
-        if (oldQuery.current !== searchQuery) {
-            oldQuery.current = searchQuery;
-            return;
-        }
-
-        setStatus(PENDING);
-     setTimeout(() => {
-        imageAPI(searchQuery, page)
-          .then(({ hits, totalHits }) => {
-            if (hits.length===0) {
-                  return Promise.reject(new Error(`There is no image with tag: ${searchQuery}`));
-              }
-            setImgFetched((prevState)=>[...prevState, ...hits]);
-            setImgTotal(totalHits);
-            setStatus(RESOLVED);
-          })
-            .catch(error => {
-                setError(error);
-                setStatus(REJECTED);
-            })
-          }, 500)
-    }, [page, searchQuery]);
-
-    useEffect(() => {
-        window.scrollTo({
-            top: document.documentElement.scrollHeight,
-            behavior: 'smooth',
+    oldQuery.current = searchQuery;
+    setStatus(PENDING);
+    setTimeout(() => {
+      imageAPI(searchQuery, page)
+        .then(({ hits, totalHits }) => {
+          if (hits.length === 0) {
+            return Promise.reject(
+              new Error(`There is no image with tag: ${searchQuery}`),
+            );
+          }
+          setImgFetched(prevState => [...prevState, ...hits]);
+          setImgTotal(totalHits);
+          setStatus(RESOLVED);
+        })
+        .catch(error => {
+          setError(error);
+          setStatus(REJECTED);
         });
-    }, [imgFetched, status]);
+    }, 500);
+  }, [page, searchQuery]);
 
-    const resetGallery = () => {
-        setStatus(IDLE);
-        setImgFetched([]);
-        setImgTotal(0);
-        setPage(1);
-        setError('');
-    };
+  useEffect(() => {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: 'smooth',
+    });
+  }, [imgFetched, status]);
 
-    const incrementPage = () => setPage(prev => prev+1);
+  const resetGallery = () => {
+    setStatus(IDLE);
+    setImgFetched([]);
+    setImgTotal(0);
+    setPage(1);
+    setError('');
+  };
 
-    const buttonLoadType = () => {
-        if(status === PENDING && page === 1) {return 'spinner'};
-        if (status === PENDING && page>1) { return 'loading' };
-        if (imgFetched.length >= imgTotal) { return 'hidden' };
-        return 'more';
-    }
-    const buttonType = buttonLoadType();
+  const incrementPage = () => setPage(prev => prev + 1);
 
-    if (status === IDLE) {
-        return <Notification text="Input tag to find images"/>
+  const buttonLoadType = () => {
+    if (status === PENDING && page === 1) {
+      return 'spinner';
     }
-    if (status === PENDING) {
-        return (
-            <>
-            <ImageGalleryList imgArray={imgFetched}/>
-            <Button type={buttonType}/>
-            </>
-        )
+    if (status === PENDING && page > 1) {
+      return 'loading';
     }
-    if (status === REJECTED) {
-        return <Notification text={error.message}/>
+    if (imgFetched.length >= imgTotal) {
+      return 'hidden';
     }
-    if (status === RESOLVED) {
-        return (
-            <>
-            <ImageGalleryList imgArray={imgFetched}/>
-            <Button type={buttonType} onClickFetch={incrementPage} />
-            </>
-        )
-    }
+    return 'more';
+  };
+  const buttonType = buttonLoadType();
+
+  if (status === IDLE) {
+    return <Notification text="Input tag to find images" />;
+  }
+  if (status === PENDING) {
+    return (
+      <>
+        <ImageGalleryList imgArray={imgFetched} />
+        <Button type={buttonType} />
+      </>
+    );
+  }
+  if (status === REJECTED) {
+    return <Notification text={error.message} />;
+  }
+  if (status === RESOLVED) {
+    return (
+      <>
+        <ImageGalleryList imgArray={imgFetched} />
+        <Button type={buttonType} onClickFetch={incrementPage} />
+      </>
+    );
+  }
 }
 
 ImageGallery.propTypes = {
-        searchQuery: PropTypes.string.isRequired,
-    }
+  searchQuery: PropTypes.string.isRequired,
+};
 
 export default ImageGallery;
